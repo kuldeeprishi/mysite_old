39388202ea0b999dd1b241a2f9ccad69cb1b6f56
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2014-02-15 16:57:36 +0530

    initial setup

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,28 @@
+from datetime import datetime
+
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from model_utils.fields import StatusField
+from model_utils import Choices
 
 
 class Blog(models.Model):
     """
     Model Representing Blog Post
     """
+    STATUS = Choices('public', 'private', 'limited')
     title = models.CharField(max_length=100)
     author = models.ForeignKey()
     body = models.TextField(blank=True, null=True)
     created = models.DateField(auto_now_add=True)
     modified = models.DateField(auto_now=True)
     published = models.DateField(default=auto_now)
+    status = StatusField()
 
     class Meta:
         verbose_name = _('Blog')
         verbose_name_plural = _('Blogs')
 
     def __unicode__(self):
-        pass
+        return self.title
